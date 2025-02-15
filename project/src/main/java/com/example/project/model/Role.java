c217c84236885908fec72fd590d8ddf633af8c5d
Abhijeet Tilekar <122143474+tilekarabhi70@users.noreply.github.com>
2025-02-15 13:31:21 +0530

    Delete project directory

--- project/src/main/java/com/example/project/model/Role.java
+++
@@ -1,6 +0,0 @@
-package com.example.project.model;
-
-public enum Role {
-	 STUDENT,
-	    ALUMNI
-}
--- project/src/main/java/com/example/project/repository/PostRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.project.repository;
-import com.example.project.model.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-@Repository
-public interface PostRepository extends JpaRepository<Post, Long> {
-}
-
